4fb708231134be7e776cef3001842a48dd14432a
Avinash Sharma <ravinashkvafs@gmail.com>
2018-03-24 23:48:05 +0530

    npm error: 4

--- app.js
+++ app.js
@@ -3,6 +3,30 @@
 //IMPORTING
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var session = require('express-session');
+//var fs = require('fs');
+var logger = require('morgan');                     //HTTP request logger - will show requests in Command Prompt
+
+
+var config = require('./config');
+
+// Load environment
+const _ENV_NAME = process.env.NAME || 'development';
+config = config[_ENV_NAME];
+
+// Mongoose Connection
+mongoose.Promise = global.Promise;
+mongoose.connect(config.mongoUrl);
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    // we're connected!
+    console.log("Connected correctly to server");
+});
 
 var app = express();
 
@@ -11,7 +35,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
+// passport config
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // catch 404 and forward to error handler
@@ -20,6 +49,11 @@ app.use(function (req, res, next) {
   err.status = 404;
   next(err);
 });
+
+//blog.info(';oiuahdf');
+//blog.error('8tgyubk80y');
+
+// error handlers
 
 // development error handler
 // will print stacktrace
