b3ca0f4da3d63a602ed8ae59bf3c3313f0ae414a
Avinash Sharma <ravinashkvafs@gmail.com>
2018-03-25 10:00:25 +0530

    rootScope use instead of headers

--- app.js
+++ app.js
@@ -9,27 +9,8 @@ var passport = require('passport');
 var session = require('express-session');
 //var fs = require('fs');
 var logger = require('morgan');                     //HTTP request logger - will show requests in Command Prompt
-var cors = require('cors');
 
 var app = express();
-
-app.use(cors());
-
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, X-Requested-With, u_info');
-
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-      res.send(200);
-    }
-    else {
-      next();
-    }
-};
-
-app.use(allowCrossDomain);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
