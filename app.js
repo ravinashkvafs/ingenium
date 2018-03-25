26d40b1439f335dd8231de596a6b2559a6ff7610
Avinash Sharma <ravinashkvafs@gmail.com>
2018-03-25 09:25:43 +0530

    cors error: 3

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(cors());
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, X-Requested-With, u_info');
 
     // intercept OPTIONS method
     if ('OPTIONS' == req.method) {
